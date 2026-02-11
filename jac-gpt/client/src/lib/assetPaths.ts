a637d3cc97a1e5bc2614ac988828a8bced5673af
udithishanka <udithishanka.s@gmail.com>
2026-02-11 22:17:21 +0530

    removed jac-gpt old

--- jac-gpt/client/src/lib/assetPaths.ts
+++
@@ -1,4 +0,0 @@
-// Asset paths for Jac syntax highlighting
-export const ONIGASM_WASM_PATH = "/onigasm.wasm";
-export const LANGUAGE_CONFIG_PATH = "/language-configuration.json";
-export const JAC_GRAMMAR_PATH = "/jac.tmLanguage.json";
